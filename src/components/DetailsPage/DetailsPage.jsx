06a1c1beb443a2f20671ea07d96750d7c160744a
colin jaworski <colinrjaworski.84@gmail.com>
2022-03-05 17:49:41 -0600

    mapping over genres and appending each in details page.

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -6,34 +6,43 @@ import { useHistory } from 'react-router-dom'
 function DetailsPage() {
     const history = useHistory();
     const dispatch = useDispatch();
-    
+
 
 
     useEffect(() => {
         console.log('in details page useEffect')
-        dispatch({ 
+        dispatch({
             type: 'FETCH_GENRES',
             payload: movie
         });
     }, []);
-    
+
     function backButton() {
         console.log('button clicked');
         history.push('/')
     };
     // const dispatch = useDispatch();
     const movie = useSelector(store => store.movie);
-    const genre = useSelector(store => store.genres)
-console.log('genere is', genre[0].name)
+    const genres = useSelector(store => store.genres)
+    console.log('genre is', genres[0])
 
     return (
         <main>
             <h1>MovieDetails</h1>
             <section className="movieDetails">
                 <h3>{movie.title}</h3>
-                {/* <h3>{genre}</h3> */}
                 <img src={movie.poster} alt={movie.title}
                     onClick={() => backButton()} />
+                <h3>Genres:</h3>
+                {genres.map((genre, id) => {
+                    return (
+                        
+                        <div key={id} >
+                            <h4>{genre.name}</h4>
+                        </div>
+                    );
+                })}
+
                 <h4>{movie.description}</h4>
                 <br />
                 <button
